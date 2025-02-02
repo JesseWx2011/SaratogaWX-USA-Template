e06c74b5a40c0407a1cc52c8fa7b6e84748aaeb2
Jesse Hasty <134172009+JesseWx2011@users.noreply.github.com>
2025-02-01 18:05:20 -0600

    Update wxforecastconfig.js

--- wxforecast-html/js/wxforecastconfig.js
+++ wxforecast-html/js/wxforecastconfig.js
@@ -1,13 +1,13 @@
 pagelocation = "Milton, FL, USA" // Location that appears on the webpage
 
-lat = "30.5819" // Latitude for Sunrise & Sunset
-long = "-86.9488"   // Longitude for Sunrise & Sunset
+lat = "30.6335" // Latitude for Sunrise & Sunset
+long = "-87.043"   // Longitude for Sunrise & Sunset
 
 
 NWSOffice = "MOB"
 NWSOfficeFull = "Mobile, AL"
 gridX = 90 // Grid X for NWS Forecasts
-gridY = 54 // Grid Y for NWS Forecasts
+gridY = 53 // Grid Y for NWS Forecasts
 
 nwsforecastplace = "Milton" // The Place that appears on the NWS Forecast Page
 nwsForecaststate = "FL"
